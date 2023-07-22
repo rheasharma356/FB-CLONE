aec867df9fe2ccf87889ab466f3e293a8f8b93f5
Rhea Sharma <rheasharma356@gmail.com>
2023-07-22 19:01:51 +0530

    new changes

--- src/Stories.js
+++ src/Stories.js
@@ -8,28 +8,28 @@ function Stories() {
   return (
     <div className='storyComponent'>
       <Story
-        image="https://scontent.fdel1-4.fna.fbcdn.net/v/t39.30808-6/315287561_5724470474312534_2568585462709619456_n.jpg?_nc_cat=105&cb=99be929b-59f725be&ccb=1-7&_nc_sid=a26aad&_nc_ohc=60HplZOGbn0AX-EIcb-&_nc_ht=scontent.fdel1-4.fna&oh=00_AfADKS2ZcRcujaaFi54O8UxLqAsoYjFSyrwdR0huwqoJqw&oe=649F81E6"
+        image="https://media.formula1.com/image/upload/content/dam/fom-website/manual/2023/Testing2023/verstappen.png.transform/6col-retina/image.png"
         profilePic="https://lh3.googleusercontent.com/a/AAcHTtd5ELY4lbZ2M8zGMKj8fHrnlbZZRaS5lx0Mu1tmeA=s432-c-no"
         title="Rhea Sharma"
         />
         <Story
-        image="https://scontent.fdel1-2.fna.fbcdn.net/v/t39.30808-6/347592364_836729864487077_1430072781860428607_n.jpg?stp=dst-jpg_p600x600&_nc_cat=101&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=sTAe9k16oQMAX-Cg4dq&_nc_ht=scontent.fdel1-2.fna&oh=00_AfCixDuGO3T7Zg47zHpX3tDO3PoIiG0T0v5iNNUQRINi5g&oe=649F85C3"
+        image="https://cdn-1.motorsport.com/static/img/archive/autosport/news/149449_1009849/s8/1009849.jpg"
         profilePic="https://upload.wikimedia.org/wikipedia/en/2/2a/Alfa_Romeo_logo.png"
         title="Alfa Romeo F1 Team"
         />
         <Story
-        image="https://scontent.fdel1-5.fna.fbcdn.net/v/t39.30808-6/356389627_649246803913526_5029345243939891688_n.jpg?_nc_cat=106&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=PAXSMg0q6RgAX_z5zPi&_nc_ht=scontent.fdel1-5.fna&oh=00_AfBC7sWcJe17X_x1CIa7tCE-9ctQn4gNnPEcapcf8fIBFA&oe=649F329A"
-        profilePic="https://scontent.fdel1-1.fna.fbcdn.net/v/t39.30808-6/347241983_803333278019735_5783196357044802262_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=WlosH-NHdfMAX_ZVLZQ&_nc_ht=scontent.fdel1-1.fna&oh=00_AfAOZptQ87G3UN3Ns8bVr3_Y-4ggFx2sPg7XJCIRoojmaQ&oe=649F1B2C"
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Mercedes_F1_Team_Garage%2C_British_GP%2C_Silverstone_2021_%2851350309945%29.jpg/1280px-Mercedes_F1_Team_Garage%2C_British_GP%2C_Silverstone_2021_%2851350309945%29.jpg"
+        profilePic="https://upload.wikimedia.org/wikipedia/commons/f/fa/Mercedes-Benz_AMG_Petronas_Formula_One_Team_Logo_Wheelsology.JPG"
         title="Mercedes-AMG Petronas F1 Team"
         />
         <Story
-        image="https://scontent.fdel1-4.fna.fbcdn.net/v/t39.30808-6/351477161_569692332004761_1572426670056540618_n.jpg?stp=dst-jpg_p960x960&_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=e3f864&_nc_ohc=ifovkcmKN44AX8DTUmz&_nc_ht=scontent.fdel1-4.fna&oh=00_AfASbTWVNXdjnRkg4Wo_XLd1_ilVDMoQBjoxOqtDrDDbvw&oe=649E1FD6"
-        profilePic="https://scontent.fdel1-1.fna.fbcdn.net/v/t39.30808-6/345652634_796914508224040_2592013330483620452_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=llDSsDNOTeIAX_dliBe&_nc_ht=scontent.fdel1-1.fna&oh=00_AfBHE5kkMW4KD8aFQ7rkexSOZPVMhSxSJ13BfuHIqVPdCQ&oe=649FC07B"
+        image="https://cdn-4.motorsport.com/images/mgl/Yv85qXE0/s1200/dennis-hauger-mp-motorsport-1.webp"
+        profilePic="https://cdn-1.motorsport.com/images/mgl/Y99JQRbY/s300/red-bull-racing-logo-1.webp"
         title="RedBull Racing"
         />
         <Story
-        image="https://scontent.fdel1-5.fna.fbcdn.net/v/t39.30808-6/354243334_740165588114199_7399860533733597833_n.jpg?_nc_cat=108&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=G8GmLK9zAVkAX_CGk8Q&_nc_ht=scontent.fdel1-5.fna&oh=00_AfB-hJuRynx058TEW-9RuBqimzwN30HoPHfsYMgK-hPOxg&oe=649F0D1F"
-        profilePic="https://scontent.fdel1-1.fna.fbcdn.net/v/t39.30808-6/342061530_767055045012624_356222935204109198_n.png?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LXOvZpbnVJgAX8yoclU&_nc_ht=scontent.fdel1-1.fna&oh=00_AfCMAi7Mx-h9VcukgmIC-1ObKuFji8wAZeAH-6zRWExk5g&oe=649E3DB8"
+        image="https://motorsportmagazine.b-cdn.net/wp-content/uploads/2023/02/Ferrari-800x450.jpg.webp"
+        profilePic="https://wallpapercave.com/dwp2x/wp11420391.png"
         title="Scuderia Ferrari"
         />
       
